0cadc4992742300f6e56f7c900160ec42cab1a32
20FTT1447 <82591923+AFIQFADHLILLAH@users.noreply.github.com>
2022-11-13 22:35:00 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,9 +37,9 @@
             @endif
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                
-                  <h1>Group 3</h1>
-                
+                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                    <h1>Group 3</h1>
+                </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
